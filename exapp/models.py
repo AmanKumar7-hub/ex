c68fd0a2fd9ebecc0f2c00fccc9ca40dc1e5c8ef
Aman Sagar <ak222760@gmail.com>
2025-01-05 19:17:44 +0530

    Added some improvements

--- exapp/models.py
+++ exapp/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+STATUS_CHOICES = [
+        ('incomplete', 'Incomplete'),
+        ('complete', 'Complete'),
+        ('pending', 'Peding'),
+]
+
 # Create your models here.
 class TaskModel(models.Model):
     title =models.CharField(max_length=100)
     description =models.CharField(max_length=200)
-    status = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='incomplete',
+    )
 
     def __str__(self):
         return self.title
